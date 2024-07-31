b6737d98b039159d6dbabbe274dad8dcb8b94ef7
Sourabh <sourabhsheokand945@gmail.com>
2024-07-31 16:45:41 +0530

    new file:   .gitignore 	modified:   Templates/home/index.html 	modified:   Templates/projects/index.html 	modified:   server.py 	modified:   static/home/css/main.css 	new file:   static/home/images/profile.ico 	new file:   static/media/resume.pdf 	new file:   static/projects/css/main.css

--- server.py
+++ server.py
@@ -12,14 +12,19 @@ eduction = [{'website':"https://www.gjust.ac.in",'university':'Guru Jambheshwar 
 
 from generate_html import make_html_highlight , list_2_html_str
 
-skills = [["Programming language",['Python','C/C++','JavaScript','SQLite','MySQL','Fortran']],["Technologies and Tools",['HTML','CSS','Nginx','JSON','LaTeX','Microcontrollers','Sensors','Origin']],["OS",['Ubuntu','Windows','macOs']],["Frameworks and Python Libraries",['Django','Flask','WebSockets','NumPy','SciPy','RESTful API']],["Version Controls",['Git']],['other','Good understanding of computer hardware and networking hardware',['computer','hardware','networking']],["other",'Good understanding of Physics and Mathematics . And working knowledge of Electronics and Chemistry',['Physics','Mathematics','Electronics','Chemistry']]]
+skills = [["Programming language",['Python','C/C++','JavaScript','SQLite','MySQL','Fortran']],["Technologies and Tools",['HTML','CSS','Nginx','JSON','LaTeX','Micro controllers','Sensors','Origin']],["OS",['Ubuntu','Windows','macOs']],["Frameworks and Python Libraries",['Django','Flask','WebSockets','NumPy','SciPy','RESTful API']],["Version Controls",['Git']],['other','Good understanding of computer hardware and networking hardware',['computer','hardware','networking']],["other",'Good understanding of Physics and Mathematics . And working knowledge of Electronics and Chemistry',['Physics','Mathematics','Electronics','Chemistry']]]
 
-projects = [{'name':'WebRoot','desc':'It is deployable server for file across the network with efficiently and securely and it uses flask for web app and nginx as the server','link':'https//github.com/sourabh945/WebRoot','skills':['Python','Flask','WebSockets','HTML','Nginx']}]
+projects = [{'name':'WebRoot','desc':'It is deployable server for file across the network with efficiently and securely and it uses flask for web app and nginx as the server','link':'https://github.com/sourabh945/WebRoot','skills':['Python','Flask','WebSockets','HTML','Nginx']},{'name':'Xanalyser','desc':'It is a XRD Data analyzing software like Origin which utilize the scipy and written in python.','link':'https://github.com/sourabh945/xanalyser','skills':['Python','SciPy','NumPy','Matplotlib']}]
+
+contact = [{'name':'Email','source':'sheokand.sourabh.anil@gmail.com','type':'email'},{'name':'Github','source':'https://github.com/sourabh945','type':'website'},{'name':'Phone','source':'+91-9306490337','type':'call'},{'name':'WhatsApp','source':'https://wa.me/+919306490337','type':'message'}]
 
 @app.route("/")
 def home():
-    return render_template("home/index.html",name='Sourabh Sheokand',about=about,eduction=eduction,skills=skills)
+    return render_template("home/index.html",name='Sourabh Sheokand',about=about,eduction=eduction,skills=skills,projects=projects,contacts=contact,source_code="https://github.com/sourabh945/Portfolio")
 
+@app.route("/projects")
+def projects_page():
+    return render_template('projects/index.html',projects=projects)
 
 if __name__ == "__main__":
     app.run(debug=True)
