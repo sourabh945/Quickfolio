97071cbdf6c9be0fc90369543313345e36d1b07b
Sourabh <125686581+sourabh945@users.noreply.github.com>
2025-05-08 12:08:34 +0530

    Update load_data.py

--- modules/load_data.py
+++ modules/load_data.py
@@ -1,6 +1,7 @@
 from json import load as ld
+from pathlib import Path
 
-data_file = "./data/data.json"
+data_file = Path(__file__).parent.parent / "data/data.json"
 
 def load() -> dict:
     with open(data_file,"r") as file:
