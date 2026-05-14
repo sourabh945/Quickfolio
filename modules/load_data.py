4c74855d927b3a0af0d97882d12db9a3bd827259
sourabh sheokand <sourabhsheokand945@gmail.com>
2026-05-15 01:34:08 +0530

    cleanup commit

--- modules/load_data.py
+++
@@ -1,8 +0,0 @@
-from json import load as ld
-from pathlib import Path
-
-data_file = Path(__file__).parent.parent / "data/data.json"
-
-def load() -> dict:
-    with open(data_file,"r") as file:
-        return ld(file)
